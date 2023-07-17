836fccd9b8da49ea087904332c3130fab22610e6
Skyfe24 <fabio.erase@gmail.com>
2023-07-17 16:40:30 +0200

    added paragraph lenghts

--- risposta.php
+++ risposta.php
@@ -1,19 +1,39 @@
 <?php
+// Recupero i valori ricevuti, quindi parola censurata e paragrafo
+$censored_word = $_GET ["censored_word"];
+$paragraph = $_GET ["paragraph"];
 
-$censored_word = $_GET;
-$paragraph = $_GET;
+//Censurare il paragrafo-> Paragrafo censurato = rimpiazzo della stringa ( parola censurata, "asterischi di censura", paragrafo)
+$censored_paragraph = str_replace($censored_word, "***", $paragraph);
 
-$censored_paragraph = str_replace($censored_word);
-
-
-
-
-
-
-
-
-
-
-
+// Quanto è lungo paragraph? lo calcoliamo col strlen
+$paragraph_lenght = strlen($paragraph);
+// Quanto è lungo censored_paragraph? lo calcoliamo
+$censored_paragraph_lenght = strlen($censored_paragraph);
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> Paragrafo originale </h2>
+    <p><?php echo $paragraph ?></p>
+    <em>       Il paragrafo originale era lungo <?= $paragraph_lenght ?> caratteri totali     </em> 
+
+    <br>
+    <h2> Paragrafo censored </h2>
+    <p><?php echo $censored_paragraph ?></p>
+    <em>       Il paragrafo censurato era lungo <?= $censored_paragraph_lenght ?> caratteri totali     </em> 
+
+
+
+
+
+
+
+</body>
+</html>
